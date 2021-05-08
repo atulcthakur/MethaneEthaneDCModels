14b4d344fc0d5204344f0f61ad93375bbf9025b0
Remsing Group <75273025+remsing-group@users.noreply.github.com>
2021-05-08 09:45:23 -0400

    Delete Simulation_Input_Files directory

--- Simulation_Input_Files/DC2Ethane/Run.sh
+++
@@ -1,2 +0,0 @@
-gmx grompp -f prod.mdp -c prod.gro -p ethane.top -o prod -maxwarn 1
-mpirun --use-hwthread-cpus -np 8 mdrun_mpi_d -deffnm prod
--- Simulation_Input_Files/DCMethane/Run.sh
+++
@@ -1,2 +0,0 @@
-gmx grompp -f prod.mdp -c prod.gro -p methane.top -o prod
-mpirun --use-hwthread-cpus -np 30 mdrun_mpi_d -deffnm prod
